ea5849c8bae67b5355834639cd1e6ec85f2b77ae
Andrew Madsen <andrew@openreelsoftware.com>
2018-12-13 23:14:12 -0700

    Optimize and shorten day 14 solution

--- Day14/Day14.swift
+++ Day14/Day14.swift
@@ -3,21 +3,7 @@ import Foundation
 let input = 825401
 
 var scoreboard = [3, 7]
-var elf1 = 0
-var elf2 = 1
-
-func combineAndCreateNewRecipe1() {
-    let sum = scoreboard[elf1] + scoreboard[elf2]
-    let digits = Array(String(sum)).map { Int(String($0))! }
-    scoreboard.append(digits[0])
-}
-
-func combineAndCreateNewRecipe2() {
-    let sum = scoreboard[elf1] + scoreboard[elf2]
-    let digits = Array(String(sum)).map { Int(String($0))! }
-    if digits.count < 2 { return }
-    scoreboard.append(digits[1])
-}
+var (elf1, elf2) = (0, 1)
 
 let inputAsArray = Array(String(input).map { Int(String($0))! })
 let inputLength = inputAsArray.count
@@ -30,31 +16,21 @@ func checkForPart2() -> Int? {
     return scoreboard.count - inputLength
 }
 
-func part1() -> String {
-    while true {    
-        combineAndCreateNewRecipe1()
-        combineAndCreateNewRecipe2()
+var gotPart1 = false
+while true {
+    let sum = scoreboard[elf1] + scoreboard[elf2]
+    if sum >= 10 {
+        scoreboard.append(sum/10)
+        if let part2 = checkForPart2() { print("part 2: \(part2)"); break; }
+    }
+    scoreboard.append(sum%10)
+    if let part2 = checkForPart2() { print("part 2: \(part2)"); break; }
 
-        elf1 = (elf1 + (scoreboard[elf1] + 1)) % scoreboard.count
-        elf2 = (elf2 + (scoreboard[elf2] + 1)) % scoreboard.count
-        
-        if scoreboard.count >= input + 10 {
-            return scoreboard[input..<input+10].map { String($0) }.joined()
-        }
+    elf1 = (elf1 + (scoreboard[elf1] + 1)) % scoreboard.count
+    elf2 = (elf2 + (scoreboard[elf2] + 1)) % scoreboard.count
+    
+    if !gotPart1 && scoreboard.count >= input + 10 {
+        print("part 1: \(scoreboard[input..<input+10].map { String($0) }.joined())") 
+        gotPart1 = true
     }
 }
-
-func part2() -> Int {
-    while true {
-        combineAndCreateNewRecipe1()
-        if let part2 = checkForPart2() { return part2 }
-        combineAndCreateNewRecipe2()
-        if let part2 = checkForPart2() { return part2 }
-
-        elf1 = (elf1 + (scoreboard[elf1] + 1)) % scoreboard.count
-        elf2 = (elf2 + (scoreboard[elf2] + 1)) % scoreboard.count
-    }
-}
-
-print(part1())
-print(part2())
